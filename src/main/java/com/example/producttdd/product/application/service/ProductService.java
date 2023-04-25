d0f42792868c552549b9f108e80410179a675bbf
leejongwoo <leejongwoo.dev@gmail.com>
2023-04-25 11:18:18 +0900

    Test: ProductService addProduct() api

--- src/main/java/com/example/producttdd/product/application/service/ProductService.java
+++ src/main/java/com/example/producttdd/product/application/service/ProductService.java
@@ -2,9 +2,15 @@ package com.example.producttdd.product.application.service;
 
 import com.example.producttdd.product.application.port.ProductPort;
 import com.example.producttdd.product.domain.Product;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
+@RequestMapping("/products")
 public class ProductService {
     private final ProductPort productPort;
 
@@ -13,8 +19,10 @@ public class ProductService {
     }
 
 
-    public void addProduct(AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody AddProductRequest request) {
         Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPort.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/test/java/com/example/producttdd/product/ProductServiceTest.java
+++ src/test/java/com/example/producttdd/product/ProductServiceTest.java
@@ -1,22 +1,38 @@
 package com.example.producttdd.product;
 
+import com.example.producttdd.ApiTest;
 import com.example.producttdd.product.application.service.AddProductRequest;
-import com.example.producttdd.product.application.service.ProductService;
 import com.example.producttdd.product.domain.DiscountPolicy;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootTest
-public class ProductServiceTest {
-
-    @Autowired
-    private ProductService productService;
+public class ProductServiceTest extends ApiTest {
 
     @Test
     void 상품등록_테스트() throws Exception {
-        final AddProductRequest request = 상품등록_요청_생성();
-        productService.addProduct(request);
+        final var request = 상품등록_요청_생성();
+
+        // API request
+        final var response = 상품등록_요청(request);
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private ExtractableResponse<Response> 상품등록_요청(AddProductRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log()
+                .all()
+                .extract();
     }
 
     private AddProductRequest 상품등록_요청_생성() {
