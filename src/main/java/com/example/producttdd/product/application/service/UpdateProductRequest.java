49ce677ef3e83be4cd4bd2d8c506839beef26bbc
leejongwoo <leejongwoo.dev@gmail.com>
2023-04-26 15:28:27 +0900

    Feat: POJO 상품수정 기능 구현, 테스트

---
+++ src/main/java/com/example/producttdd/product/application/service/UpdateProductRequest.java
@@ -0,0 +1,12 @@
+package com.example.producttdd.product.application.service;
+
+import com.example.producttdd.product.domain.DiscountPolicy;
+import org.springframework.util.Assert;
+
+public record UpdateProductRequest(String name, int price, DiscountPolicy discountPolicy) {
+    public UpdateProductRequest {
+        Assert.hasText(name, "상품명은 필수입니다.");
+        Assert.isTrue(price > 0, "가격은 0보다 커야합니다.");
+        Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+    }
+}
---
+++ src/test/java/com/example/producttdd/product/ProductServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.producttdd.product;
+
+import com.example.producttdd.product.application.port.ProductPort;
+import com.example.producttdd.product.application.service.ProductService;
+import com.example.producttdd.product.application.service.UpdateProductRequest;
+import com.example.producttdd.product.domain.DiscountPolicy;
+import com.example.producttdd.product.domain.Product;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProductServiceTest {
+
+    private ProductService productService;
+    private StubProductPort productPort = new StubProductPort();;
+
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService(productPort);
+    }
+
+    @Test
+    void 상품수정_테스트() throws Exception {
+        final Long productId = 1L;
+        final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
+        Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
+        productPort.getProduct_will_return = product;
+
+        productService.updateProduct(productId, request);
+
+        Assertions.assertThat(product.getName()).isEqualTo("상품 수정");
+        Assertions.assertThat(product.getPrice()).isEqualTo(2000);
+    }
+
+    private static class StubProductPort implements ProductPort {
+        public Product getProduct_will_return;
+
+        @Override
+        public void save(Product product) {
+        }
+
+        @Override
+        public Product getProduct(Long productId) {
+            return getProduct_will_return;
+        }
+    }
+
+}
