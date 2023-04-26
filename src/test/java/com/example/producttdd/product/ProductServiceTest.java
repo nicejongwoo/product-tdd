a3f11c99f93e0769a475613bba32f5f809a7b60e
leejongwoo <leejongwoo.dev@gmail.com>
2023-04-26 15:54:47 +0900

    Refact: SpringBootTest 상품수정

--- src/test/java/com/example/producttdd/product/ProductServiceTest.java
+++ src/test/java/com/example/producttdd/product/ProductServiceTest.java
@@ -1,48 +1,34 @@
 package com.example.producttdd.product;
 
-import com.example.producttdd.product.application.port.ProductPort;
+import com.example.producttdd.product.application.service.GetProductResponse;
 import com.example.producttdd.product.application.service.ProductService;
 import com.example.producttdd.product.application.service.UpdateProductRequest;
 import com.example.producttdd.product.domain.DiscountPolicy;
-import com.example.producttdd.product.domain.Product;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
+@SpringBootTest
 public class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
-    private StubProductPort productPort = new StubProductPort();;
-
-    @BeforeEach
-    void setUp() {
-        productService = new ProductService(productPort);
-    }
 
     @Test
     void 상품수정_테스트() throws Exception {
+        productService.addProduct(ProductSteps.상품등록_요청_생성());
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
-        Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        productPort.getProduct_will_return = product;
 
         productService.updateProduct(productId, request);
 
-        Assertions.assertThat(product.getName()).isEqualTo("상품 수정");
-        Assertions.assertThat(product.getPrice()).isEqualTo(2000);
-    }
+        ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        GetProductResponse body = response.getBody();
 
-    private static class StubProductPort implements ProductPort {
-        public Product getProduct_will_return;
-
-        @Override
-        public void save(Product product) {
-        }
-
-        @Override
-        public Product getProduct(Long productId) {
-            return getProduct_will_return;
-        }
+        Assertions.assertThat(body.name()).isEqualTo("상품 수정");
+        Assertions.assertThat(body.price()).isEqualTo(2000);
     }
 
 }
