a9f911909a50b6cf436c02a55f26d7d883719646
leejongwoo <leejongwoo.dev@gmail.com>
2023-04-29 16:10:47 +0900

    Refact: OrderSteps

--- src/test/java/com/example/producttdd/order/OrderApiTest.java
+++ src/test/java/com/example/producttdd/order/OrderApiTest.java
@@ -2,13 +2,9 @@ package com.example.producttdd.order;
 
 import com.example.producttdd.ApiTest;
 import com.example.producttdd.product.ProductSteps;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 public class OrderApiTest extends ApiTest {
 
@@ -17,25 +13,11 @@ public class OrderApiTest extends ApiTest {
 
         ProductSteps.상품등록_요청(ProductSteps.상품등록_요청_생성());
 
-        CreateOrderRequest request = 상품주문_요청_생성();
+        var request = OrderSteps.상품주문_요청_생성();
 
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/orders")
-                .then()
-                .log().all()
-                .extract();
+        var response = OrderSteps.상품주문_요청(request);
 
         Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-
-    private CreateOrderRequest 상품주문_요청_생성() {
-        Long productId = 1L;
-        int quantity = 2;
-        return new CreateOrderRequest(productId, quantity);
     }
 
 }
