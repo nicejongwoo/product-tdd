7a4d4795355c4fdf2a2f7f7924f04c83c740ef6f
leejongwoo <leejongwoo.dev@gmail.com>
2023-04-30 20:17:34 +0900

    Fix: @Transactional

--- src/main/java/com/example/producttdd/order/application/service/OrderService.java
+++ src/main/java/com/example/producttdd/order/application/service/OrderService.java
@@ -5,6 +5,7 @@ import com.example.producttdd.order.domain.Order;
 import com.example.producttdd.product.domain.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ class OrderService {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<Void> createOrder(@RequestBody CreateOrderRequest request) {
         Product product = orderPort.getProduct(request.productId());
         final Order order = new Order(product, request.quantity());
